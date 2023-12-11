51838a819d1d774a1b5f18c726128b48f4bf30c7
4lex42 <a.laroy@students.ephec.be>
2023-12-11 09:54:38 +0100

    Add PRE/POST

--- main.py
+++ main.py
@@ -9,13 +9,25 @@ import pyzipper
 
 class ZipFolderWithPassword:
     def __init__(self):
+        """
+        Initialize ZipFolderWithPassword instance.
+
+        PRE: None
+        POST: Initializes the instance with empty strings for folder_path,
+              destination_folder, zip_filename, and password.
+        """
         self.folder_path = ""
         self.destination_folder = ""
         self.zip_filename = ""
         self.password = ""
 
     def get_user_input(self):
-        # get the inputs with GUI
+        """
+        Collect user input using a GUI.
+
+        PRE: None
+        POST: Returns True if all inputs are received; False otherwise.
+        """
         root = tk.Tk()
         root.withdraw()
 
@@ -33,9 +45,15 @@ class ZipFolderWithPassword:
             return False
 
         root.destroy()
+        return True
 
     def zip_folder(self):
-        # zip the folder
+        """
+        Zip the folder with password protection.
+
+        PRE: Assumes that get_user_input has been called successfully.
+        POST: Prints an error message if an exception occurs during zipping.
+        """
         try:
             zip_path = os.path.join(self.destination_folder, self.zip_filename)
             with pyzipper.AESZipFile(zip_path, 'w', compression=pyzipper.ZIP_DEFLATED,
@@ -58,7 +76,13 @@ class ZipFolderWithPassword:
             print(f"An error occurred: {e}")
 
     def sort_files_by_name(self):
-        # sort files by alphabetic
+        """
+        Sort files alphabetically.
+
+        PRE: Assumes that get_user_input has been called successfully.
+        POST: Returns a list of file paths sorted alphabetically.
+              Prints an error message if an exception occurs during sorting.
+        """
         try:
             # Liste tous les fichiers dans le dossier
             files = sorted(os.listdir(self.folder_path))
@@ -72,6 +96,13 @@ class ZipFolderWithPassword:
 
 class CommandLineZipper:
     def __init__(self, folder_path, destination_folder, zip_filename, password):
+        """
+        Initialize CommandLineZipper instance.
+
+        PRE: None
+        POST: Initializes the instance with the provided values for
+              folder_path, destination_folder, zip_filename, and password.
+        """
         self.zip_handler = ZipFolderWithPassword()
         self.zip_handler.folder_path = folder_path
         self.zip_handler.destination_folder = destination_folder
@@ -79,7 +110,13 @@ class CommandLineZipper:
         self.zip_handler.password = password
 
     def execute_zip_command(self):
-        # execute the zip
+        """
+        Execute the zip command in command-line mode.
+
+        PRE: Assumes that command-line arguments are provided.
+        POST: Prints an error message if an exception occurs during zipping.
+              Prints a success message if the operation is completed successfully.
+        """
         try:
             self.zip_handler.zip_folder()
             print("Zip operation completed successfully.")
@@ -88,6 +125,15 @@ class CommandLineZipper:
 
 
 def main():
+    """
+    Main entry point for the script.
+
+    PRE: None
+    POST: If command-line arguments (folder_path, destination_folder, zip_filename, and password)
+          are provided, it executes the zip operation in command-line mode.
+          If no command-line arguments are provided, it initiates the GUI mode
+          to collect user input and perform the zip operation.
+    """
     parser = argparse.ArgumentParser(description="Zip a folder with password protection. \n")
     parser.add_argument("--folder_path", help="Path to the folder to be zipped. \n")
     parser.add_argument("--destination_folder", help="Path to the destination folder. \n")
@@ -103,8 +149,9 @@ def main():
     else:
         # GUI mode
         zip_handler = ZipFolderWithPassword()
-        zip_handler.get_user_input()
-        zip_handler.zip_folder()
+        success = zip_handler.get_user_input()
+        if success:
+            zip_handler.zip_folder()
 
 
 if __name__ == "__main__":
